f928f1f96105a3bae60a825fa12c0146b0588d1c
Peter Nguyen <peternguyenftw@gmail.com>
2023-03-25 17:52:40 -0400

    Updated dates and dummy data

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import NewExpense from './components/NewExpense/NewExpense';
 const DUMMY_DATA = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: 'Masks',
+    amount: 67.49,
+    date: new Date(2021, 4, 11),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New Computer', amount: 1599.99, date: new Date(2022, 3, 14) },
   {
     id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: 'Phone payments',
+    amount: 598.67,
+    date: new Date(2022, 4, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    title: 'Tuition',
+    amount: 4500,
+    date: new Date(2022, 9, 12),
   },
 ];
 
